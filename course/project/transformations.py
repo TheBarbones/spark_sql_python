991b099760d64cf699cabefe135c86daef7714fd
moises osorio <moises.osorio@live.com>
2023-10-03 20:14:51 +1100

    adding filter and writing functions

--- course/project/transformations.py
+++ course/project/transformations.py
@@ -57,24 +57,38 @@ def aggregations(list_input: list[DataFrame]):
     parquet_df = list_input[2]
     agg_df = parquet_df.groupby(col("species")).agg(
         {
-            "species": "max",
-            "species": "min",
+            "sepal_length": "max",
+            "sepal_width": "min",
+            "petal_length": "count",
+            "petal_width": "avg",
         }
     )
 
-    agg_df.show()
-
 
 def filtering_and_sorting(list_input: list[DataFrame]):
-    pass
+    parquet_df = list_input[2]
+    filtered_df = parquet_df.filter(col("species") == lit("setosa"))
 
 
 def joining_data(list_input: list[DataFrame]):
-    pass
+    joining_one_df = list_input[0]
+    joining_two_df = list_input[2]
+
+    joining_data_df = joining_one_df.alias("j1").join(
+        joining_two_df.alias("j2"), col("j1.id") == col("j2.id"), "inner"
+    )
+
+    joining_data_df = joining_one_df.join(joining_two_df, ["id"], "inner")
+
+    condition = [col("j1.id") == col("j2.id")]
+    joining_data_df = joining_one_df.alias("j1").join(
+        joining_two_df.alias("j2"), condition, "left"
+    )
 
 
 def writing_data(list_input: list[DataFrame]):
-    pass
+    writing_df = list_input[2]
+    writing_df.write.mode("overwrite").parquet("course/data/output/write_exercise/")
 
 
 def caching_data(list_input: list[DataFrame]):
