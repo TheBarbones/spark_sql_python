6208e9d4c7b48fc2fb281c8e0069849b12b35725
moises osorio <moises.osorio@live.com>
2023-10-06 17:52:10 +1100

    adding options in loading data

--- course/project/ingestion_schema_evolution.py
+++ course/project/ingestion_schema_evolution.py
@@ -8,15 +8,29 @@ def loading_data(spark: SparkSession):
     - JSON
     - PARQUET
     - ORC
+
+    Options:
+        - header
+        - inferSchema
+        - delimiter
+        - encoding
+        - quote
+        - escape
+        - multiLine
+        - ignoreLeadingWhiteSpace
+        - ignoreTrailingWhiteSpace
+
+
+
+
     :param spark:
     :return:
     """
-    csv_df = spark.read.option("header", "true").csv("course/data/input/iris_2023.xls")
-    json_df = spark.read.option("header", "true").json(
-        "course/data/input/iris_2023.json"
-    )
+    parm_path = "course/data/input/"
+    csv_df = spark.read.option("header", "true").csv(f"{parm_path}iris_2023.xls")
+    json_df = spark.read.option("header", "true").json(f"{parm_path}iris_2023.json")
     parquet_df = spark.read.option("header", "true").parquet(
-        "course/data/input/iris_2023.parquet"
+        f"{parm_path}iris_2023.parquet"
     )
 
     return [csv_df, json_df, parquet_df]
